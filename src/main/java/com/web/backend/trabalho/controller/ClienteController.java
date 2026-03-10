715067c6c6394e2c3fa1e9a8c6df8e68c67dd98d
Rikelmy Lopes <rikelmylopes899@gmail.com>
2026-03-10 13:25:34 -0300

    criado as rotas do cliente

--- src/main/java/com/web/backend/trabalho/controller/ClienteController.java
+++ src/main/java/com/web/backend/trabalho/controller/ClienteController.java
@@ -1,18 +1,41 @@
 package com.web.backend.trabalho.controller;
 
+import com.web.backend.trabalho.model.entities.Cliente;
 import com.web.backend.trabalho.model.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@RestController()
+@RequestMapping("/clientes")
 public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @PostMapping("/user")
-    public String getUser() {
-        return "Nome: Fulano \n" +
-                "Idade: 22";
+
+    @PostMapping
+    public void cadastrar(@RequestBody Cliente cliente) {
+        this.clienteService.cadastrar(cliente);
+    }
+
+    @GetMapping("/{id}")
+    public Cliente buscarPorId(@PathVariable Long id) {
+        Cliente cliente = this.clienteService.buscarPorId(id);
+        if (cliente == null) {
+            throw new RuntimeException("Cliente não encontrado");
+        }
+
+        return cliente;
+    }
+
+    @GetMapping
+    public List<Cliente> buscarTodos() {
+        return this.clienteService.buscarTodos();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable final Long id) {
+        this.clienteService.deletar(id);
     }
 }
